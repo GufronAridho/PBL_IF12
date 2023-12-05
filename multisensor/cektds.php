ca89323a7df160a831de7ac3cb3350087c97ceda
GufronAridho <gufronaridho4@gmail.com>
2023-12-05 15:39:36 +0700

    Rev 2

--- multisensor/cektds.php
+++ multisensor/cektds.php
@@ -1,6 +1,6 @@
 <?php
-    $koneksi = mysqli_connect("localhost", "root", "", "dbmultisensor");
-    $sql = mysqli_query($koneksi, "select * from tb_sensor order by ID desc");
+    $konek = mysqli_connect("localhost", "root", "", "dbmultisensor");
+    $sql = mysqli_query($konek, "select * from tb_sensor order by ID desc");
     $data = mysqli_fetch_array($sql);
     $tds = $data['tds'];
 
--- multisensor/send_data.php
+++ multisensor/send_data.php
@@ -1,11 +1,11 @@
 <?php
-    $koneksi = mysqli_connect("localhost", "root", "", "dbmultisensor");
+    $konek = mysqli_connect("localhost", "root", "", "dbmultisensor");
     $ph = $_GET['ph'];
     $tds = $_GET['tds'];
     $temp = $_GET['temp'];
 
 
-    $save = mysqli_query($koneksi,"insert into tb_sensor(ph, tds, temp)values('$ph','$tds','$temp')");
+    $save = mysqli_query($konek,"insert into tb_sensor(ph, tds, temp)values('$ph','$tds','$temp')");
     
     if($save)
         echo "Send Success";
