13564fc67f920036ab2302e14d153e5ed2e651a4
GufronAridho <119670148+GufronAridho@users.noreply.github.com>
2023-12-14 15:05:40 +0700

    Delete multisensor/cektds.php

--- multisensor/cektds.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost", "root", "", "dbmultisensor");
-    $sql = mysqli_query($conn, "select * from tb_sensor order by ID desc");
-    $data = mysqli_fetch_array($sql);
-    $tds = $data['tds'];
-
-    if ($tds == "") $tds = 0;
-    echo $tds;
-?>
